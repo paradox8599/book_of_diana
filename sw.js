2e32418353a54664e8c6a34079f281279bb88828
paradox8599 <paradox8599@outlook.com>
2026-01-06 10:40:38 +0800

    enhance: improve SEO, PWA support, and service worker robustness

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('my-pwa-cache-v1').then((cache) => {
+    caches.open('my-pwa-cache-v3').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
+        '/manifest.json',
+        '/icon_512.png',
         '/bg.webp',
         '/bg_mobile.webp'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== 'my-pwa-cache-v3')
+            .map(key => caches.delete(key))
+      );
     })
   );
 });
